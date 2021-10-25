6941631ca58d6fa28bf4042ec5e6ac5bc68530ae
Francisco <fnasif@fi.uba.ar>
2021-10-25 20:42:03 -0300

    Fixed fix loading

--- src/pages/Cursos/Cursos.jsx
+++ src/pages/Cursos/Cursos.jsx
@@ -12,7 +12,7 @@ const Cursos = () => {
   useEffect(() => {
     obtenerCursos().then((res) => {
       setCursos(res);
-      setSuccess(!success);
+      setSuccess((success) => !success);
     });
   }, []);
 
